30bdf7a0c1bdf4b9d6b5184652db1015d466d008
Palash Singh Raghuwanshi <singhpalash0@gmail.com>
2021-05-10 01:44:25 +0530

    Edit spotify redirect uri

--- app/views.py
+++ app/views.py
@@ -19,7 +19,7 @@ MESSAGE_TAGS = {
 
 os.environ["SPOTIPY_CLIENT_ID"] = "72de6f3abdc24383a58e4b56cfb14e14"
 os.environ["SPOTIPY_CLIENT_SECRET"] = "f5f9fbe5d9a64d11896e7dc42bd901ed"
-os.environ["SPOTIPY_REDIRECT_URI"] = "http://127.0.0.1:8000/"
+os.environ["SPOTIPY_REDIRECT_URI"] = "https://playlistd.herokuapp.com/"
 
 
 
